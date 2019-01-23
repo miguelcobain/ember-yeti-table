7e34346aa2f83f4ee93f0a17ab4c673a075d3c96
Miguel Andrade <jmandrade0@gmail.com>
2019-01-23 00:25:33 +0000

    fix required decorator

--- tests/dummy/app/pods/components/material-pagination/component.js
+++ tests/dummy/app/pods/components/material-pagination/component.js
@@ -2,12 +2,10 @@ import Component from '@ember/component';
 
 import { tagName } from '@ember-decorators/component';
 import { argument } from '@ember-decorators/argument';
-import { required } from '@ember-decorators/argument/validation';
 
 @tagName('')
 export default class MaterialPagination extends Component {
 
   @argument
-  @required
   table;
 }
