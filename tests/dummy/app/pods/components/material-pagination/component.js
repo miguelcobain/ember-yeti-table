6d6212eb94c42e6349e5d05d524e9383d41f85fc
Miguel Andrade <jmandrade0@gmail.com>
2019-12-30 16:42:49 +0000

    fix material pagination

--- tests/dummy/app/pods/components/material-pagination/component.js
+++ tests/dummy/app/pods/components/material-pagination/component.js
@@ -2,10 +2,17 @@ import Component from '@ember/component';
 
 import { tagName } from '@ember-decorators/component';
 
+import { action } from '@ember/object';
+
 @tagName('')
 class MaterialPagination extends Component {
 
   table;
+
+  @action
+  changePageSize(ev) {
+    this.table.actions.changePageSize(ev.target.value);
+  }
 }
 
 export default MaterialPagination;
