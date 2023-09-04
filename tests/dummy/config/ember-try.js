dc2c17998c962738376ca37cc785ff2f841370c3
Brian Gantzler <bgantzler@yahoo.com>
2023-09-03 22:42:29 -0400

    Updated ember-try for pnpm and 4.12

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -5,49 +5,58 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
+    usePnpm: true,
     scenarios: [
       {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
-          },
-        },
+            'ember-source': '~4.4.0'
+          }
+        }
       },
       {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
-          },
-        },
+            'ember-source': '~4.8.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0'
+          }
+        }
       },
       {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release'),
-          },
-        },
+            'ember-source': await getChannelURL('release')
+          }
+        }
       },
       {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
+            'ember-source': await getChannelURL('beta')
+          }
+        }
       },
       {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('canary'),
-          },
-        },
+            'ember-source': await getChannelURL('canary')
+          }
+        }
       },
       embroiderSafe(),
-      embroiderOptimized(),
-    ],
+      embroiderOptimized()
+    ]
   };
 };
